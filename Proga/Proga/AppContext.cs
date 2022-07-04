c52f945aa0ac1ffcc6d2e12d4655eaa999089779
FANTOM-AC <super.cfr2016@yandex.ru>
2022-07-04 20:02:47 +0300

    +++++++++

---
+++ Proga/Proga/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Proga
+{
+    class AppContext : DbContext
+    {
+        public DbSet<Metestan> metestan { get; set; }
+
+        public AppContext() : base("DefaultConnection") { }
+    }
+}
